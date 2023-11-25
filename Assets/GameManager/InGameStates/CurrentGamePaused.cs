55fea374e4ce72228f9bb467afa25ab35874669d
ercutto <ercumentti81@gmail.com>
2023-11-25 18:05:16 +0200

    sprite library and in game states

--- Assets/GameManager/InGameStates/CurrentGamePaused.cs
+++ Assets/GameManager/InGameStates/CurrentGamePaused.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CurrentGamePaused : MonoBehaviour
 {
-   public void Pause()
+    public TMP_Text text;
+    private string pause = "Pause";
+    private string unPause = "Play";
+    bool isPaused=false;
+    public void Pause()
     {
-        
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            text.text = pause;
+            isPaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            isPaused=false;
+            text.text = unPause;
+        }
     }
 }
--- Assets/GameManager/InGameStates/ScoreBoardController.cs
+++ Assets/GameManager/InGameStates/ScoreBoardController.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ScoreBoardController:MonoBehaviour
 {
     public void UpdateBoard()
     {
-
+        Debug.Log("board update");
     }
   
 }
